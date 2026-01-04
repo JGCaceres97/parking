f5b365c49e209272671c91cd0972f5eb05e99185
Joseph Cáceres <jgcaceres97@gmail.com>
2026-01-04 14:18:13 -0600

    FIX: Improve docker integration for multi-database backend

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,25 +1,47 @@
 #!/bin/sh
 
-DB_DRIVER="mysql"
-DB_CONN="$MYSQL_USER:$MYSQL_PASSWORD@tcp($DB_HOST:$DB_PORT)/$MYSQL_DATABASE?parseTime=true"
-GOOSE_DIR="./migrations"
+# Salida del script automática en caso de error
+set -e
 
-# 1. Esperar a que la Base de Datos esté disponible
-echo "⏳ Esperando que MySQL esté disponible en $DB_HOST:$DB_PORT..."
-until nc -z "$DB_HOST" "$DB_PORT"; do
-  sleep 1
-done
-echo "✅ MySQL listo. Iniciando migraciones..."
+MIGRATIONS_DIR="./migrations/$DB_DRIVER"
 
-# 2. Ejecutar las migraciones con Goose
-/usr/bin/goose "$DB_DRIVER" "$DB_CONN" up -dir "$GOOSE_DIR"
-
-if [ $? -ne 0 ]; then
-  echo "❌ ERROR: Las migraciones de Goose fallaron. Saliendo del contenedor."
+if [ ! -d "$MIGRATIONS_DIR" ]; then
+  echo "❌ Directorio de migraciones no existe: $MIGRATIONS_DIR"
   exit 1
 fi
 
-echo "✅ Migraciones completadas. Iniciando la aplicación Go..."
+# 1. Construir DSN dependiendo el driver
+case "$DB_DRIVER" in
+  sqlite)
+    if [ -z "$SQLITE_DSN" ]; then
+      echo "❌ SQLITE_DSN no definido"
+      exit 1
+    fi
 
-# 4. Ejecutar el comando principal de la aplicación (definido en CMD)
+    DB_CONN="$SQLITE_DSN"
+    ;;
+
+  mysql)
+    DB_CONN="$MYSQL_USER:$MYSQL_PASSWORD@tcp($DB_HOST:$DB_PORT)/$MYSQL_DATABASE?parseTime=true&loc=UTC"
+
+    # 1.1. Esperar a que la Base de Datos esté disponible
+    echo "⏳ Esperando que MySQL esté disponible en $DB_HOST:$DB_PORT..."
+    until nc -z "$DB_HOST" "$DB_PORT"; do
+      sleep 1
+    done
+    echo "✅ MySQL listo."
+    ;;
+
+  *)
+    echo "❌ DB_DRIVER no soportado: $DB_DRIVER"
+    exit 1
+    ;;
+esac
+
+# 2. Ejecutar las migraciones
+echo "⬆️ Iniciando migraciones desde $MIGRATIONS_DIR..."
+/usr/bin/goose "$DB_DRIVER" "$DB_CONN" up -dir "$MIGRATIONS_DIR"
+
+# 3. Ejecutar el comando principal de la aplicación (definido en CMD)
+echo "✅ Migraciones completadas. Iniciando la aplicación Go..."
 exec "$@"
